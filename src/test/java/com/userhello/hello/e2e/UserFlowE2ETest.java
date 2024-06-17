75d80554911b7be8ea95c97194aa29bfb0eac0f2
reza <m.motallebi73@gmail.com>
2024-06-17 08:56:11 +0200

    Tests done: 12 > Selenium

--- src/test/java/com/userhello/hello/e2e/UserFlowE2ETest.java
+++ src/test/java/com/userhello/hello/e2e/UserFlowE2ETest.java
@@ -25,47 +25,62 @@ public class UserFlowE2ETest {
         try {
             driver.get("http://localhost:8080/signup");
 
-            // Wait for the username field to be interactable
+            System.out.println("Opened signup page");
+
             WebElement unameField = wait.until(ExpectedConditions.elementToBeClickable(By.name("uname")));
             unameField.sendKeys("testuser");
+            System.out.println("Entered username");
 
             WebElement nameField = driver.findElement(By.id("name"));
             nameField.sendKeys("Test");
+            System.out.println("Entered name");
 
             WebElement familyNameField = driver.findElement(By.id("familyName"));
             familyNameField.sendKeys("User");
+            System.out.println("Entered family name");
 
             WebElement birthdateField = driver.findElement(By.id("birthdate"));
             birthdateField.sendKeys("2000-01-01");
+            System.out.println("Entered birthdate");
 
             WebElement birthPlaceField = driver.findElement(By.id("birthPlace"));
             birthPlaceField.sendKeys("City");
+            System.out.println("Entered birthplace");
 
             WebElement currentCountryField = driver.findElement(By.id("currentCountry"));
             currentCountryField.sendKeys("Country");
+            System.out.println("Entered current country");
 
             WebElement currentCityField = driver.findElement(By.id("currentCity"));
             currentCityField.sendKeys("City");
+            System.out.println("Entered current city");
 
             WebElement schoolNameField = driver.findElement(By.id("schoolName"));
             schoolNameField.sendKeys("School");
+            System.out.println("Entered school name");
 
             WebElement gpaField = driver.findElement(By.id("gpa"));
             gpaField.sendKeys("4.0");
+            System.out.println("Entered GPA");
 
             WebElement phoneField = driver.findElement(By.id("phone"));
             phoneField.sendKeys("1234567890");
+            System.out.println("Entered phone");
 
             WebElement emailField = driver.findElement(By.id("email"));
             emailField.sendKeys("test@example.com");
+            System.out.println("Entered email");
 
-            // Wait for the submit button to be interactable
             WebElement submitButton = wait.until(ExpectedConditions.elementToBeClickable(By.id("submit")));
             submitButton.click();
+            System.out.println("Clicked submit button");
 
-            // Add assertions or further steps as necessary
-            assertTrue(driver.getPageSource().contains("Welcome, testuser"));
+            boolean isSignupSuccessful = wait.until(ExpectedConditions.textToBePresentInElementLocated(By.tagName("body"), "Welcome, Test!"));
+            assertTrue(isSignupSuccessful, "Signup was not successful");
 
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.out.println("Test failed due to exception: " + e.getMessage());
         } finally {
             driver.quit();
         }
