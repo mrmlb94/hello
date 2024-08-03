80daf48865b775c9d6924a11da6f4b5fb618b455
mrmlb <m.motallebi73@gmail.com>
2024-08-03 19:01:22 +0200

    a small change

--- src/test/java/com/userhello/hello/e2e/CombinedE2ETest.java
+++ src/test/java/com/userhello/hello/e2e/CombinedE2ETest.java
@@ -3,32 +3,25 @@ package com.userhello.hello.e2e;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
-
-import java.time.Duration;import org.junit.jupiter.api.*;
+import java.time.Duration;
+import org.junit.jupiter.api.*;
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
-
-import java.time.Duration;
 import java.util.List;
 import java.util.UUID;
-
 import static org.junit.jupiter.api.Assertions.*;
 
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
-public class CombinedE2ETest {
+class CombinedE2ETest {
     private static WebDriver driver;
     private static WebDriverWait wait;
-    public final static int TIMEOUT = 10;
-
+    static final int TIMEOUT = 10;
 
     @BeforeAll
-    public void setup() {
+    void setup() {
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--remote-allow-origins=*");
         options.addArguments("--no-sandbox");
@@ -43,7 +36,7 @@ public class CombinedE2ETest {
 
     @Test
     @Order(1)
-    public void testSignup() {
+    void testSignup() {
         driver.get("http://localhost:8080/signup");
         driver.navigate().refresh();
 
@@ -67,7 +60,7 @@ public class CombinedE2ETest {
 
     @Test
     @Order(2)
-    public void testLogin() {
+    void testLogin() {
         driver.get("http://localhost:8080/login");
 
         WebElement usernameField = wait.until(ExpectedConditions.presenceOfElementLocated(By.name("uname")));
@@ -83,7 +76,7 @@ public class CombinedE2ETest {
 
     @Test
     @Order(3)
-    public void testQuizAndLogout() {
+    void testQuizAndLogout() {
         driver.get("http://localhost:8080/quiz");
 
         wait.until(ExpectedConditions.presenceOfElementLocated(By.id("vivaldi1"))).click();
@@ -100,7 +93,7 @@ public class CombinedE2ETest {
 
     @Test
     @Order(4)
-    public void testUserFlow() {
+    void testUserFlow() {
         driver.get("http://localhost:8080/users");
 
         WebElement addNewUserLink = wait.until(ExpectedConditions.elementToBeClickable(By.linkText("Add New User")));
@@ -143,7 +136,7 @@ public class CombinedE2ETest {
     }
 
     @AfterAll
-    public static void tearDown() {
+    static void tearDown() {
         if (driver != null) {
             driver.quit();
         }
