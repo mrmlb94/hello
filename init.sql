1b847ec95c37c47454a80a9887da2098d9cbadb4
Mohammadreza Motallebi <56297052+mrmlb94@users.noreply.github.com>
2025-03-14 20:14:08 +0100

    Update

--- init.sql
+++ init.sql
@@ -7,16 +7,3 @@ BEGIN
 END
 $$;
 
-\c user_db;
-
-DO
-$$
-BEGIN
-    IF NOT EXISTS (SELECT 1 FROM pg_sequences WHERE sequencename = 'user_seq') THEN
-        CREATE SEQUENCE user_seq
-        START WITH 1
-        INCREMENT BY 1  -- ⚠️ Set increment size to 1
-        NO CYCLE;
-    END IF;
-END
-$$;
